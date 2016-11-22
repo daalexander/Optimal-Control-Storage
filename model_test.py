10a0a5c68f1e9a720d4b97db71b72441dbc2b7a5
Daniel Alexander <alexanderdaniel@live.de>
2016-11-22 12:13:21 +0100

    replace masflows with PSOS and PC_1

--- model_test.py
+++ model_test.py
@@ -2,34 +2,30 @@
 
 
 ### flows
-m0in= PSOS
-m2out= PSOS
-m1out= PSOS
+PSOS= PSOS
+PSOS*VSHP_OP= PSOS
+PSOS*VSHP_CL= PSOS
 
-if CCH_1 =1:				# ACM is working or we are heating
-			m0out=30		# Angenommener Wert für ACM muss noch abgestimmt werden
-			m1in=30
-			m2in=30;
-		else 
-			m0out=PC_1
-			m1in=PC_1
-			m2in=PC_1;
+
+PC_1=PC_1
+PC_1*VSHS_CL=PC_1
+PC_1*VSHS_OP=PC_1;
 
 
 #first Layer
-m0 * dotT0 = m0in * TSOS - m0out * TSH0 - (m0plus + m0in - m0out) * TSH0 + m0plus * TSH2 # m0in is PSOS # m0out is PC_1 or A_IN_1
-#m0minus = m0plus + m0in - m0out 
+m0 * dotT0 = PSOS * TSOS - PC_1 * TSH0 - (m0plus + PSOS - PC_1) * TSH0 + m0plus * TSH2 # PSOS is PSOS # PC_1 is PC_1 or A_IN_1
+#m0minus = m0plus + PSOS - PC_1 
 
 #second Layer
-m2 * dotT2 = -m2out * TSH2 + m2in  * TCO_1 + (m0plus + m0in - m0out) * TSH0 - m0plus * TSH2  \
-- (-m2out +m0in -m0out +m2in +m2plus) * TSH2 + m2plus * TSH3 # m2out is PSOS, m2in is PC_1 or A_IN_1
-#m2minus = -m2out +m0in -m0out +m2in +m2plus
+m2 * dotT2 = -PSOS*VSHP_OP * TSH2 + PC_1*VSHS_OP  * TCO_1 + (m0plus + PSOS - PC_1) * TSH0 - m0plus * TSH2  \
+- (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 + m2plus * TSH3 # PSOS*VSHP_OP is PSOS, PC_1*VSHS_OP is PC_1 or A_IN_1
+#m2minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus
 
 #third Layer
-m3 * dotT3 = (-m2out +m0in -m0out +m2in +m2plus) * TSH2 - m2plus * TSH3 - (-m2out +m0in -m0out +m2in +m2plus -m2plus +m3plus) * TSH1 + m3plus * TSH1
-#m3minus = -m2out +m0in -m0out +m2in +m2plus -m2plus +m3plus
+m3 * dotT3 = (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 - m2plus * TSH3 - (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH1 + m3plus * TSH1
+#m3minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus
 
 #fourth Layer
-m4 * dotT4 = -m1out * TSH1 + (-m2out +m0in -m0out +m2in +m2plus -m2plus +m3plus) * TSH3 - m3plus * TSH1 + m1in * TCO_1
+m4 * dotT4 = -PSOS*VSHP_CL * TSH1 + (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH3 - m3plus * TSH1 + PC_1*VSHS_CL * TCO_1
 
 
