db557b1482cfdcae251124887fbbf375ca5382de
Daniel Alexander <alexanderdaniel@live.de>
2016-11-22 13:14:44 +0100

    first solution

--- model_test.py
+++ model_test.py
@@ -2,30 +2,22 @@
 
 
 ### flows
-PSOS= PSOS
-PSOS*VSHP_OP= PSOS
-PSOS*VSHP_CL= PSOS
-
-
-PC_1=PC_1
-PC_1*VSHS_CL=PC_1
-PC_1*VSHS_OP=PC_1;
 
 
 #first Layer
-m0 * dotT0 = PSOS * TSOS - PC_1 * TSH0 - (m0plus + PSOS - PC_1) * TSH0 + m0plus * TSH2 # PSOS is PSOS # PC_1 is PC_1 or A_IN_1
+dotT0 = 1/m0 * (PSOS * TSOS - PC_1 * TSH0 - (m0plus + PSOS - PC_1) * TSH0 + m0plus * TSH2) 
 #m0minus = m0plus + PSOS - PC_1 
 
 #second Layer
-m2 * dotT2 = -PSOS*VSHP_OP * TSH2 + PC_1*VSHS_OP  * TCO_1 + (m0plus + PSOS - PC_1) * TSH0 - m0plus * TSH2  \
-- (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 + m2plus * TSH3 # PSOS*VSHP_OP is PSOS, PC_1*VSHS_OP is PC_1 or A_IN_1
+dotT2 = 1/m2 * (-PSOS*VSHP_OP * TSH2 + PC_1*VSHS_OP  * TCO_1 + (m0plus + PSOS - PC_1) * TSH0 - m0plus * TSH2  \
+- (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 + m2plus * TSH3)
 #m2minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus
 
 #third Layer
-m3 * dotT3 = (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 - m2plus * TSH3 - (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH1 + m3plus * TSH1
+dotT3 = 1/m3 * ((-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 - m2plus * TSH3 - (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH1 + m3plus * TSH1)
 #m3minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus
 
 #fourth Layer
-m4 * dotT4 = -PSOS*VSHP_CL * TSH1 + (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH3 - m3plus * TSH1 + PC_1*VSHS_CL * TCO_1
+dotT1 = 1/m1(-PSOS*VSHP_CL * TSH1 + (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH3 - m3plus * TSH1 + PC_1*VSHS_CL * TCO_1)
 
 
--- storage_model.py
+++ storage_model.py
@@ -11,7 +11,6 @@ import casadi as ca
 cp_water = 4182.0
 
 
-
 # States
 
 x = ca.MX.sym("x", 4)
@@ -23,115 +22,48 @@ TSH1  = x[3]
 
 # Parameters
 
-# # p = ca.MX.sym("p", 1)
-
-# u_glass = 0.2 # 2.0
-# u_wall =  0.0786715 # see: room_pe_step1_night.py
-
-# window_transmission = 0.00195234 # see: room_pe_step2_day.py
-
-# u_radiator = 0.286806
-
+# p = ca.MX.sym("p", 1)
 
 # pinit = ca.vertcat([u_radiator_init])
-
-T_ref = 23.0
-mdot_radiator_min = 0.0
-mdot_radiator_max = 260.0 / 3600.0
-mdot_radiator_init = 0.0
-T_radiator_min = 70.0
-T_radiator_max = 70.0 
-T_radiator_init = T_radiator_min
 
 
 # Controls
 
-u = ca.MX.sym("u", 4)
+u = ca.MX.sym("u", 8)
 
-environment_temperature = u[0]
-qdot_sun = u[1]
-mdot_radiator_in = u[2]
-feed_temperature = u[3]
-
-
-# Heat flows radiator
-
-# Heat flow CVs
-
-qdot_cv1 = u_radiator * exchange_surface_cv * \
-    (radiator_cv_temperature[0] - room_temperature)
-
-dt_cv1 = (1.0 / (cv_m * cp_water)) * \
-    (mdot_radiator_in * cp_water * \
-    (feed_temperature - radiator_cv_temperature[0]) - \
-    qdot_cv1)
+PSOS = u[0]
+PC_1 = u[1]
+m0minus = u[2]
+m0plus = u[3]
+m2minus = u[4]
+m2plus = u[5]
+m3minus = u[6]
+m3plus = u[7]
 
 
-qdot_cv2 = u_radiator * exchange_surface_cv * \
-    (radiator_cv_temperature[1] - room_temperature)
+# Massflows storage
 
-dt_cv2 = (1.0 / (cv_m * cp_water)) * \
-    (mdot_radiator_in * cp_water * \
-    (radiator_cv_temperature[0] - radiator_cv_temperature[1]) - \
-    qdot_cv2)
+#first Layer
+dotT0 = 1/m0 * (PSOS * TSOS - PC_1 * TSH0 - (m0plus + PSOS - PC_1) * TSH0 + m0plus * TSH2) 
+#m0minus = m0plus + PSOS - PC_1 
 
+#second Layer
+dotT2 = 1/m2 * (-PSOS*VSHP_OP * TSH2 + PC_1*VSHS_OP  * TCO_1 + (m0plus + PSOS - PC_1) * TSH0 - m0plus * TSH2  \
+    - (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 + m2plus * TSH3)
+#m2minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus
 
-qdot_cv3 = u_radiator * exchange_surface_cv * \
-    (radiator_cv_temperature[2] - room_temperature)
+#third Layer
+dotT3 = 1/m3 * ((-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 - m2plus * TSH3 \
+    - (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH1 + m3plus * TSH1)
+#m3minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus
 
-dt_cv3 = (1.0 / (cv_m * cp_water)) * \
-    (mdot_radiator_in * cp_water * \
-    (radiator_cv_temperature[1] - radiator_cv_temperature[2]) - \
-    qdot_cv3)
+#fourth Layer
+dotT1 = 1/m1(-PSOS*VSHP_CL * TSH1 + (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH3 - m3plus * TSH1 + PC_1*VSHS_CL * TCO_1)
 
-
-qdot_cv4 = u_radiator * exchange_surface_cv * \
-    (radiator_cv_temperature[3] - room_temperature)
-
-dt_cv4 = (1.0 / (cv_m * cp_water)) * \
-    (mdot_radiator_in * cp_water * \
-    (radiator_cv_temperature[2] - radiator_cv_temperature[3]) - \
-    qdot_cv4)
-
-
-qdot_cv5 = u_radiator * exchange_surface_cv * \
-    (radiator_cv_temperature[4] - room_temperature)
-
-dt_cv5 = (1.0 / (cv_m * cp_water)) * \
-    (mdot_radiator_in * cp_water * \
-    (radiator_cv_temperature[3] - radiator_cv_temperature[4]) - \
-    qdot_cv5)
-
-
-
-# Sum of radiator flows
-
-qdot_radiator = sum([qdot_cv1, qdot_cv2, qdot_cv3, qdot_cv4, qdot_cv5]) 
-
-
-# Heat flows room
-
-environment_qdot_window = u_glass * window_surface * \
-   (environment_temperature - room_temperature)
-  
-environment_qdot_wall = u_wall * environment_surface * \
-   (environment_temperature - room_temperature)
-
-environment_qdot = environment_qdot_window + environment_qdot_wall
-
-qdot_loss = environment_qdot
-
-qdot_sun_effective = 0.7 * window_surface * window_transmission * qdot_sun
-
-
-dt_room = (1.0 / (room_mass * cp_air)) * \
-   (qdot_loss + qdot_sun_effective + qdot_radiator + qdot_otherfactors)
 
 
 dxdt = ca.vertcat( \
-    dt_room, \
-    dt_cv1, \
-    dt_cv2, \
-    dt_cv3, \
-    dt_cv4, \
-    dt_cv5)
+    dotT0, \
+    dotT2, \
+    dotT3, \
+    dotT1)
