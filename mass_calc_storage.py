2bf96d51c75d215076f707907ee4ee1d047f575a
Alexander Daniel <Alexander Daniel>
2017-01-04 20:13:39 +0100

    ergänzende schritte in der berechnung

--- mass_calc_storage.py
+++ mass_calc_storage.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import numpy as NP
-import casadi as ca
+# import casadi as ca
 
 data = pd.read_table("data20161015.csv", \
     delimiter=";")#, index_col=0)
 
-list1 = data["TSOS"]
-list2 = data["PC_1"]
+# list1 = data["PSOS"]
+# list2 = data["PC_1"]
+data['m0'] = data.PSOS - data.PC_1 
 
-list3 = list1 - list2
+data_m0 = data['m0']
+
+data_m0plus = data_m0[(data_m0['m0'] < 0)]
+data_minus = data_m0[(data_m0['m0'] > 0)]
+
+data.insert('data_minus', 'data_m0plus')
+
+print data.head()
 
 # list3 = list3.astype(float)
 
-# data.to_csv('/home/da/Master/Thesis/massdata.csv')
+data.to_csv('/home/da/Master/Thesis/massdata.csv')
