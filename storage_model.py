fe70bb912de2569d80bb761798dbe6685f0a2172
Daniel Alexander <alexanderdaniel@live.de>
2017-01-05 13:58:32 +0100

    angepasst

--- storage_model.py
+++ storage_model.py
@@ -10,7 +10,7 @@ import casiopeia as cp
 
 
 cp_water = 4182.0
-
+layer = 4
 
 # States
 
@@ -42,35 +42,61 @@ m3minus = u[6]
 m3plus = u[7]
 TSOS = u[8]
 TCO_1 = u[9]
+# VSHP_OP = u[10]
+# VSHP_CL = u[11]
+# VSHS_OP = u[12]
+# VSHS_CL = u[13]
 
-m = 2000.0 / 4
+m = 2000.0 / layer
 
 # Massflows storage
 
+## without VSHS_CL/OP and VSHP_CL/OP
 #first Layer
 dotT0 = 1.0/m * (PSOS * TSOS - PC_1 * TSH0 - (m0plus + PSOS - PC_1) * TSH0 + m0plus * TSH2) 
 #m0minus = m0plus + PSOS - PC_1 
 
 #second Layer
-dotT2 = 1.0/m * (-PSOS*VSHP_OP * TSH2 + PC_1*VSHS_OP  * TCO_1 + (m0plus + PSOS - PC_1) * TSH0 - m0plus * TSH2  \
-    - (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 + m2plus * TSH3)
-#m2minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus
+dotT2 = 1.0/m * ( -PSOS * TSH2 + PC_1 * TCO_1 + (m0plus + PSOS - PC_1) * TSH0 - m0plus * TSH2  \
+    - (-PSOS + PSOS - PC_1 + PC_1 + m2plus) * TSH2 + m2plus * TSH3)
+#m2minus = -PSOS +PSOS -PC_1 +PC_1 +m2plus
 
 #third Layer
-dotT3 = 1.0/m * ((-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus) * TSH2 - m2plus * TSH3 \
-    - (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH1 + m3plus * TSH1)
-#m3minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus
+dotT3 = 1.0/m * ((-PSOS + PSOS - PC_1 + PC_1 + m2plus) * TSH2 - m2plus * TSH3 \
+    - (-PSOS + PSOS - PC_1 + PC_1  + m3plus) * TSH1 + m3plus * TSH1)
+#m3minus = -PSOS +PSOS -PC_1 +PC_1  +m3plus
 
 #fourth Layer
-dotT1 = 1.0/m * (-PSOS*VSHP_CL * TSH1 + (-PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus) * TSH3 - m3plus * TSH1 + PC_1*VSHS_CL * TCO_1)
+dotT1 = 1.0/m * (-PSOS * TSH1 + (-PSOS + PSOS - PC_1 + PC_1  + m3plus) * TSH3 - m3plus * TSH1 + PC_1 * TCO_1)
+
+#=================================================================================================================================================
+## with VSHS_CL/OP and VSHP_CL/OP
+# #first Layer
+# dotT0 = 1.0/m * (PSOS * TSOS - PC_1 * TSH0 - (m0plus + PSOS - PC_1) * TSH0 + m0plus * TSH2) 
+# #m0minus = m0plus + PSOS - PC_1 
+
+# #second Layer
+# dotT2 = 1.0/m * ( -PSOS * VSHP_OP * TSH2 + PC_1 * VSHS_OP * TCO_1 + (m0plus + PSOS - PC_1) * TSH0 - m0plus * TSH2  \
+#     - (-PSOS * VSHP_OP + PSOS - PC_1 + PC_1 * VSHS_OP + m2plus) * TSH2 + m2plus * TSH3)
+# #m2minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP +m2plus
+
+# #third Layer
+# dotT3 = 1.0/m * ((-PSOS * VSHP_OP + PSOS - PC_1 + PC_1 * VSHS_OP + m2plus) * TSH2 - m2plus * TSH3 \
+#     - (-PSOS * VSHP_OP + PSOS - PC_1 + PC_1 * VSHS_OP  + m3plus) * TSH1 + m3plus * TSH1)
+# #m3minus = -PSOS*VSHP_OP +PSOS -PC_1 +PC_1*VSHS_OP  +m3plus
+
+# #fourth Layer
+# dotT1 = 1.0/m * (-PSOS * VSHP_CL * TSH1 + (-PSOS * VSHP_OP + PSOS - PC_1 + PC_1 * VSHS_OP  + m3plus) * TSH3 - m3plus * TSH1 + PC_1 * VSHS_CL * TCO_1)
+#=================================================================================================================================================
+
 
 #ODE
 
-f = ca.vertcat( \
+f = ca.vertcat([ \
     dotT0, \
     dotT2, \
     dotT3, \
-    dotT1)
+    dotT1])
 
 # phi = x
 
@@ -83,9 +109,6 @@ f = ca.vertcat( \
 # int_end = 1000
 # steps = 1
 
-# # int_start = 10800
-# # int_end = 11500
-# # steps = 1
 
 # TSH0 = x[0]
 # TSH2  = x[1]
@@ -93,7 +116,7 @@ f = ca.vertcat( \
 # TSH1  = x[3]
 
 # data = pd.read_table("data20161015.csv", \
-#     delimiter=";", index_col=0)
+#     delimiter=",", index_col=0)
 
 # for k,e in enumerate(int_start):
 
@@ -119,7 +142,16 @@ f = ca.vertcat( \
 
 #     udata_9 = data["TCO_1"][e:int_end[k]-1:int_step].values
 
-#     udata = ca.horzcat([udata_0, udata_1, udata_2, udata_3, udata_4, udata_5, udata_6, udata_7, udata_8])
+#     # udata_10 = data["VSHP_OP"][e:int_end[k]-1:int_step].values
+
+#     # udata_11 = data["VSHP_CL"][e:int_end[k]-1:int_step].values
+
+#     # udata_12 = data["VSHS_OP"][e:int_end[k]-1:int_step].values
+
+#     # udata_13 = data["VSHS_CL"][e:int_end[k]-1:int_step].values
+
+#     udata = ca.horzcat([udata_0, udata_1, udata_2, udata_3, udata_4, udata_5, udata_6, udata_7, udata_8, udata_9])#, \
+#         # udata_10, udata_11, udata_12, udata_13])
 
 #     #t_outlet = data["Outlet"][e:int_end[k]-1:int_step].values #from pe_step3
 
